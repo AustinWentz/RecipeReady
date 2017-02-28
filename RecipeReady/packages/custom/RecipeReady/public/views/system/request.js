b0c546402121c6cd9ecb8cb4b81c2b2a6f7c4742
pike4 <pike4@purdue.edu>
2017-02-28 16:31:49 -0500

    views

---
+++ RecipeReady/packages/custom/RecipeReady/public/views/system/request.js
@@ -0,0 +1,31 @@
+//Implement server API cals below
+
+//TODO:
+getIngredients()
+{
+    
+}
+
+//TODO
+getRecipes()
+{
+    
+}
+
+//TODO: get the user's saved recipes
+getRecipes(user)
+{
+    
+}
+
+//TODO get the ingredients in the user's pantry
+getPantry(user)
+{
+    
+}
+
+//TODO: get a list of the user's dietary restrictions
+getDietaryRestrictions(user)
+{
+    
+}
